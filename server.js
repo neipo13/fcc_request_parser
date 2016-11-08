0235104ebdf5c9ee466a1c4a421e411b9d8d59ca
Andrew Napierkowski <andrew.napierkowski@gmail.com>
2016-11-08 03:01:38 +0000

    v1

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res){
+    var userAgent = req.headers['user-agent'];
+    var ip = req.ip || req.headers["accept-language"] || req.connection.remoteAddress;
+    var lang = req.acceptedLanguages[0];
+    //var firstParen = userAgent.indexOf('(');
+    var soft = userAgent.substring(userAgent.indexOf('(') + 1, userAgent.indexOf(')'));
+    var ret = {
+        "ipaddress": ip,
+        "language": lang,
+        "software": soft
+    }
+    res.send(ret);
+});
+
+app.listen(process.env.PORT || 8080, function(){
+    console.log('listening on 8080');
+});
