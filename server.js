064af10e423fe819cf6a55db6b40bd9fdcaf48de
Andrew Napierkowski <andrew.napierkowski@gmail.com>
2016-11-08 03:37:52 +0000

    language parsing issues

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
 
+var first =  function(array) {  
+      if (array == null){
+        return null;
+      } 
+      return array[0];  
+  };  
+
 app.get('/', function(req, res){
     var userAgent = req.headers['user-agent'];
     var ip = req.ip || req.headers["accept-language"] || req.connection.remoteAddress;
-    var lang = req.acceptedLanguages[0] || req.headers["accept-language"];
+    var lang = req.headers["accept-language"];
+    lang = lang.substring(0, lang.indexOf(',') || lang.indexOf(';'));
     var soft = userAgent.substring(userAgent.indexOf('(') + 1, userAgent.indexOf(')'));
     var ret = {
         "ipaddress": ip,
@@ -17,3 +25,4 @@ app.get('/', function(req, res){
 app.listen(process.env.PORT || 8080, function(){
     console.log('listening on 8080');
 });
+
