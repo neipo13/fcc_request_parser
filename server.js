27cedceb03b94df2649455c5b776f201939bfdd9
Andrew Napierkowski <andrew.napierkowski@gmail.com>
2016-11-08 03:14:43 +0000

    error info 3

--- server.js
+++ server.js
@@ -16,11 +16,7 @@ app.get('/', function(req, res){
 });
 
 app.use(function(err, req, res, next){
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message, 
-        error: err
-    });
+    res.send(err.message);
 });
 
 app.listen(process.env.PORT || 8080, function(){
